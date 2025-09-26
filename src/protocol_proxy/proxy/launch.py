706dae44626bed365656f0f7e061732df3786a56
David M. Raker <david.raker@pnnl.gov>
2025-09-26 11:51:38 -0700

    Fixed passing of tokens to proxy process.

--- src/protocol_proxy/proxy/launch.py
+++ src/protocol_proxy/proxy/launch.py
@@ -26,8 +26,8 @@ def launch(launcher_func: Callable):
     parser = proxy_command_parser()
     parser, proxy_runner = launcher_func(parser)
     opts = parser.parse_args()
-    proxy_token = UUID(hex=sys.stdin.buffer.read(32).hex())
-    manager_token = UUID(hex=sys.stdin.buffer.read(32).hex())
+    proxy_token = UUID(hex=sys.stdin.buffer.read(32).decode('utf8'))
+    manager_token = UUID(hex=sys.stdin.buffer.read(32).decode('utf8'))
     if iscoroutinefunction(proxy_runner):
         run(proxy_runner(token=proxy_token, manager_token=manager_token, **vars(opts)))
     else:
